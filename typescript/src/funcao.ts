a39db6c8a372670f616d4965cfc6de5a2e18a9d3
Morenodev085 <moreno.ferreira11@gmail.com>
2024-09-30 12:55:11 -0300

    Add files via upload

---
+++ typescript/src/funcao.ts
@@ -0,0 +1,10 @@
+function somar(a: number, b: number): number{
+    return a + b;
+}
+
+const somar2 =(a: number, b:number): number => a + b 
+
+
+function dizerOla(nome:string) {
+    return `ola ${nome}`;
+}
---
+++ typescript/src/tipos_basicos.ts
@@ -0,0 +1,12 @@
+let estachovanedo: boolean = false
+
+let idade: number = 25
+
+let altura: number =1.60
+
+const nacinalidae: string = 'brasileiro'
+
+const colegas: string [] = ['pedro', 'andre', ]
+
+
+const soma: number[] = [1, 2]
